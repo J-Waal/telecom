a71a9e1957e76bcf480ce45f187c027430a8f922
J-Waal <J.R.deWaal@student.tudelft.nl>
2022-05-10 21:22:20 +0200

    Update mobius6.py
    made a start on question 2

--- mobius6.py
+++ mobius6.py
@@ -1,10 +1,13 @@
 from statistics import NormalDist
+import math as np
 
 #question 1
 print("question 1")
 
 noise_mean = 0
 noise_rms = 1.4 # found in part a
+# WARNING! MADE A MISTAKE USING VARIANCE INSTEAD OF SD
+# also rms is not equal to the variance when the mean is not 0
 false_alarm_probability_percent = 6 # %, found in part a
 airplane_reflected = 3.7 # volt, found in part b
 detection_probability_percent = 99 # %, found in part v
@@ -25,5 +28,19 @@ print("")
 
 #question 2
 print("question 2")
+
+a_amplitude = 11.5 # V
+v_negative_t = -3.85 # V
+v_positive_t = 4.25 # V
+noise_sd = 1 # V
+
 # part a
+signal_power = a_amplitude**2
+noise_power = noise_sd**2
+snr_db = 10*np.log10(signal_power/noise_power)
+print(f"SNR: {snr_db} dB")
 # part b
+p_error1 = 1-NormalDist(-a_amplitude,noise_sd).cdf(v_negative_t)
+p_error2 = NormalDist(a_amplitude,noise_sd).cdf(v_positive_t)
+
+print(p_error1,p_error2)
